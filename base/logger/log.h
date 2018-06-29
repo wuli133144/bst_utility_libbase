d6df6ac4a627608432790c8658f8636c17244afd
wuli133144 <1683358846@qq.com>
2018-06-29 12:33:43 +0800

    log4cxx simple

--- base/logger/log.h
+++ base/logger/log.h
@@ -2,73 +2,49 @@
 
 
 
-#ifndef BOOST_LOG_INIT_TAIRAN_HPP
-#define BOOST_LOG_INIT_TAIRAN_HPP
+#ifndef LOG4CXX_H
+#define LOG4CXX_H
 
-#include <iostream>
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/log/support/date_time.hpp>
-#include <boost/log/common.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/expressions/keyword.hpp>
 
-#include <boost/log/attributes.hpp>
-#include <boost/log/attributes/timer.hpp>
-#include <boost/log/sources/logger.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sinks/sync_frontend.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
+#include <log4cxx/logger.h>  
+#include <log4cxx/basicconfigurator.h>  
+#include <log4cxx/propertyconfigurator.h>  
+#include <log4cxx/helpers/exception.h>  
+#include <iostream> 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/console.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
 
-#include <boost/log/attributes/named_scope.hpp>
+using namespace log4cxx;
+using namespace log4cxx::helpers;
 
-namespace logging = boost::log;
-namespace attrs = boost::log::attributes;
-namespace src = boost::log::sources;
-namespace sinks = boost::log::sinks;
-namespace expr = boost::log::expressions;
-namespace keywords = boost::log::keywords;
 
-enum SeverityLevel
-{
-    Log_Info,
-    Log_Notice,
-    Log_Debug,
-    Log_Warning,
-    Log_Error,
-    Log_Fatal
+
+class Clogger{
+public:
+
+      static Clogger *getInstance(){
+	  	 if(!Instance_){
+		 	Instance_=new Clogger;
+		 }
+		 return Instance_;
+	  }
+      LoggerPtr get(){return m_logger_file;}
+	  
+private:
+	 LoggerPtr m_logger_file;
+	 LoggerPtr m_logger_console;
+     Clogger();
+	 
+     static Clogger * Instance_;
+
 };
 
-// The formatting logic for the severity level
-template< typename CharT, typename TraitsT >
-inline std::basic_ostream< CharT, TraitsT >& operator<< (
-    std::basic_ostream< CharT, TraitsT >& strm, SeverityLevel lvl)
-{
-    static const char* const str[] =
-    {
-        "Info",
-        "Notice",
-        "Debug",
-        "Warning",
-        "Error",
-        "Fatal"
-    };
-    if (static_cast< std::size_t >(lvl) < (sizeof(str) / sizeof(*str)))
-        strm << str[lvl];
-    else
-        strm << static_cast< int >(lvl);
-    return strm;
-}
 
-BOOST_LOG_ATTRIBUTE_KEYWORD(log_severity, "Severity", SeverityLevel)
-BOOST_LOG_ATTRIBUTE_KEYWORD(log_timestamp, "TimeStamp", boost::posix_time::ptime)
-BOOST_LOG_ATTRIBUTE_KEYWORD(log_uptime, "Uptime", attrs::timer::value_type)
-BOOST_LOG_ATTRIBUTE_KEYWORD(log_scope, "Scope", attrs::named_scope::value_type)
 
-void g_InitLog();
+
 
 #endif
 
