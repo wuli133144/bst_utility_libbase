d8228a887685ff059ad42949f4ddc00fb25d755a
wuli133144 <1683358846@qq.com>
2018-07-05 15:41:26 +0800

    client.cc

---
+++ base/socket_utils/client.cc
@@ -0,0 +1,170 @@
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <thread>
+#include <boost/asio.hpp>
+
+#include "public_define.h"
+#include "ImPduBase.h"
+#include "BaseSocket.h"
+#include "StringUtils.h"
+
+#include "IM.Basedefine.pb.h"
+#include "IM.Live.pb.h"
+
+
+
+
+using asio::ip::tcp;
+using namespace IM::BaseDefine;
+
+
+typedef std::deque<CImPdu> chat_message_queue;
+
+class chat_client
+{
+public:
+  chat_client(asio::io_service& io_service,
+      const tcp::resolver::results_type& endpoints)
+    : io_context_(io_service),
+      socket_(io_service)
+  {
+    do_connect(endpoints);
+  }
+
+  void write(const CImPdu * msg)
+  {
+    asio::post(io_context_,
+        [this, msg]()
+        {
+          bool write_in_progress = !write_msgs_.empty();
+          write_msgs_.push_back(msg);
+          if (!write_in_progress)
+          {
+            do_write();
+          }
+        });
+  }
+
+  void close()
+  {
+    asio::post(io_context_, [this]() { socket_.close(); });
+  }
+
+private:
+  void do_connect(const tcp::resolver::results_type& endpoints)
+  {
+    asio::async_connect(socket_, endpoints,
+        [this](std::error_code ec, tcp::endpoint)
+        {
+          if (!ec)
+          {
+            do_read_header();
+          }
+        });
+  }
+
+  void do_read_header()
+  {
+    asio::async_read(socket_,
+        asio::buffer(read_msg_.GetBuffer(), read_msg_.GetBodyLength()),
+        [this](std::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec && read_msg_.decode_header())
+          {
+            do_read_body();
+          }
+          else
+          {
+            socket_.close();
+          }
+        });
+  }
+
+  void do_read_body()
+  {
+    asio::async_read(socket_,
+        asio::buffer(read_msg_.body(), read_msg_.body_length()),
+        [this](std::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            std::cout.write(read_msg_.body(), read_msg_.body_length());
+            std::cout << "\n";
+            do_read_header();
+          }
+          else
+          {
+            socket_.close();
+          }
+        });
+  }
+
+  void do_write()
+  {
+    asio::async_write(socket_,
+        asio::buffer(write_msgs_.front().data(),
+          write_msgs_.front().length()),
+        [this](std::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            write_msgs_.pop_front();
+            if (!write_msgs_.empty())
+            {
+              do_write();
+            }
+          }
+          else
+          {
+            socket_.close();
+          }
+        });
+  }
+
+private:
+  asio::io_service& io_context_;
+  tcp::socket socket_;
+  CImPdu read_msg_;
+  chat_message_queue write_msgs_;
+};
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cerr << "Usage: chat_client <host> <port>\n";
+      return 1;
+    }
+
+    asio::io_service io_service;
+
+    tcp::resolver resolver(io_service);
+    auto endpoints = resolver.resolve(argv[1], argv[2]);
+    chat_client c(io_service, endpoints);
+
+    std::thread t([&io_service](){ io_service.run(); });
+
+    char line[2048]={0};
+    while (std::cin.getline(line, 2048))
+    {
+      CImPdu msg;
+	  IM::Live::Get
+      msg.SetPBMsg();
+      std::memcpy(msg.body(), line, msg.body_length());
+      msg.encode_header();
+      c.write(msg);
+    }
+
+    c.close();
+    t.join();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
