8890d267d45b2c812387ef463b78f0334dbf868f
wuyujie@sunlands.com <wuyujie@sunlands.com>
2018-08-08 18:24:25 +0800

    some modify test dismiss transform permision of group

--- base/socket_utils/client.cc
+++ base/socket_utils/client.cc
@@ -1,7 +1,8 @@
 #include <cstdlib>
 #include <deque>
 #include <iostream>
-#include <thread>
+#include <thread>
+
 #include <memory>
 #include <boost/asio.hpp>
 
@@ -10,22 +11,20 @@
 #include "ImPduBase.h"
 #include "BaseSocket.h"
 #include "StringUtils.h"
-
 #include "IM.BaseDefine.pb.h"
-#include "IM.Live.pb.h"
 #include "IM.RPC.pb.h"
 #include "IM.Other.pb.h"
 #include "IM.Message.pb.h"
-
+#include "IM.Group.pb.h"
 #include "ConfigFileReader.h"
 
 using boost::asio::ip::tcp;
 using namespace IM::BaseDefine;
-using namespace IM::Live;
+using namespace IM::Group;
+//using namespace IM::Live;
 
 
 typedef std::deque<CImPdu> chat_message_queue;
-
 
 
 class chat_client:public std::enable_shared_from_this<chat_client>
@@ -72,7 +71,7 @@ public:
   //!  structure need a current transport object,it contains 
   //!  ip port,functionname and so on,but this client test
   //!  can't create tarsinfo.
-  void CreateLiveRoom(uint32_t live_id,uint32_t user_id,uint32_t capacitys){
+  /*void CreateLiveRoom(uint32_t live_id,uint32_t user_id,uint32_t capacitys){
      CImPdu pPdu;
 	 IM::RPC::IMRPCCreateLiveChatRoomReq msg;
      msg.set_live_id(live_id);
@@ -90,7 +89,7 @@ public:
 	 
 	 write(pPdu);
       
-  }
+  }*/
 
   void close()
   {
@@ -240,139 +239,150 @@ int main(int argc, char* argv[])
 	std::thread f([&]{
 	while(1){
 		
-	    IM::Other::IMHeartBeat msg;
-        CImPdu pdu;
-        pdu.SetPBMsg(&msg);
-        pdu.SetServiceId(SID_OTHER);
-        pdu.SetCommandId(CID_OTHER_HEARTBEAT);
-		
-	    socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
+		IM::Other::IMHeartBeat msg;
+		CImPdu pdu;
+		pdu.SetPBMsg(&msg);
+		pdu.SetServiceId(SID_OTHER);
+		pdu.SetCommandId(CID_OTHER_HEARTBEAT);
+
+		socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
 		usleep(5000*1000);}
-	
+
 	});
-     std::thread work([&]{
-	       std::cout<<"test msg ack Cache function"<<std::endl;
-		    
-		    IM::Message::IMMsgDataAck msg;
-			msg.set_user_id(3662414);
-			msg.set_session_id(7899);
-			msg.set_msg_id(12);
-			msg.set_create_time((uint64_t)time(NULL));
-			msg.set_session_type(2);
-			msg.set_result_code(0);
-			
-			
-			CImPdu pdu;
-		    pdu.SetPBMsg(&msg);
-		    pdu.SetServiceId(SID_MSG);                            
-		    pdu.SetCommandId(CID_MSG_DATA_ACK);
-			socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
-		   
-           
-	 });
 
-	      std::thread work1([&]{
-	       std::cout<<"test msg ack Cache function"<<std::endl;
-		    
-		    IM::Message::IMMsgDataAck msg;
-			msg.set_user_id(3662415);
-			msg.set_session_id(7899);
-			msg.set_msg_id(12);
+	#if 0
+	std::thread work([&]{
+		
+		std::cout<<"test  IMGroupMemberDismissReq msg ack Cache function"<<std::endl;
+		IM::Group::IMGroupMemberDismissReq msg;    
+		//creator 
+		//msg.set_creator_id(1856727);
+		
+		//manager
+		msg.set_creator_id(1856728);
+		//1856728
+		msg.set_group_id(6144);
+		msg.set_reason("test creator_id 1856727");
+		//msg.set_result_code(0);
 
-			msg.set_create_time((uint64_t)time(NULL));
-			msg.set_session_type(2);
-			msg.set_result_code(0);
-			
-			
-			CImPdu pdu;
-		    pdu.SetPBMsg(&msg);
-		    pdu.SetServiceId(SID_MSG);
-		    pdu.SetCommandId(CID_MSG_DATA_ACK);
-			socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
-		   
-           
-	 });
 
-	    std::thread work2([&]{
-	       std::cout<<"test msg ack Cache function"<<std::endl;
-		    
-		    IM::Message::IMMsgDataAck msg;
-			msg.set_user_id(3662413);
-			msg.set_session_id(7899);
-			msg.set_msg_id(12);
+		CImPdu pdu;
+		pdu.SetPBMsg(&msg);
+		pdu.SetServiceId(SID_GROUP);                            
+		pdu.SetCommandId(CID_GROUP_MEMBER_DISMISS_REQUEST);
+		socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
 
-			msg.set_create_time((uint64_t)time(NULL));
-			msg.set_session_type(2);
-			msg.set_result_code(0);
-			
-			
-			CImPdu pdu;
-		    pdu.SetPBMsg(&msg);
-		    pdu.SetServiceId(SID_MSG);
-		    pdu.SetCommandId(CID_MSG_DATA_ACK);
-			socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
-		   
-           
-	 });
-	 //1816394 1858937 1859909 1860721
+		//usleep(1000*100);
+        //vector<char>recvbuffer(1024,0);
+		unsigned char *recvbuffer=new unsigned char[1024];
+		memset(recvbuffer,0,sizeof(unsigned char)*1024);
+		
+		socket.receive(boost::asio::buffer(recvbuffer,1024));
 
-	    std::thread work3([&]{
-	       std::cout<<"test msg ack Cache function"<<std::endl;
-		    
-		    IM::Message::IMMsgDataAck msg;
-			msg.set_user_id(3662412);
-			msg.set_session_id(7899);
-			msg.set_msg_id(12);
+		IM::Group::IMGroupMemberDismissResp msgresp;
+        
+	    msgresp.ParseFromArray(recvbuffer+16, 1024);
 
-			msg.set_create_time((uint64_t)time(NULL));
-			msg.set_session_type(2);
-			msg.set_result_code(0);
-			
-			
-			CImPdu pdu;
-		    pdu.SetPBMsg(&msg);
-		    pdu.SetServiceId(SID_MSG);
-		    pdu.SetCommandId(CID_MSG_DATA_ACK);
-			socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
-		   
-           
-	 });
-	  std::thread work4([&]{
-	       std::cout<<"test msg ack Cache function"<<std::endl;
-		    
-		    IM::Message::IMMsgDataAck msg;
-			msg.set_user_id(5001);
-			msg.set_session_id(7899);
-			msg.set_msg_id(12);
+		//std::cout<<recvbuffer<<std::endl;
 
-			msg.set_create_time((uint64_t)time(NULL));
-			msg.set_session_type(2);
-			msg.set_result_code(0);
-			
-			
-			CImPdu pdu;
-		    pdu.SetPBMsg(&msg);
-		    pdu.SetServiceId(SID_MSG);
-		    pdu.SetCommandId(CID_MSG_DATA_ACK);
-			socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
-		   
-           
-	 });
+		std::cout<<std::endl<<"#############################response start#######################"<<std::endl;
+		std::cout<<"group  name :  "<<msgresp.group_name()<<std::endl;
+		std::cout<<"result code : "<<msgresp.result_code()<<std::endl;
+		std::cout<<"group    id : "<<msgresp.group_id()<<std::endl;
+		std::cout<<"creator  id : "<<msgresp.creator_id()<<std::endl;
+
+        for(auto i=0;i < msgresp.member_list_size();i++)
+        {
+           std::cout<<"member index "<< i<<" imid "<<msgresp.member_list(i)<<" in group [ "<< msgresp.group_id()<<" ]"<<std::endl;
+        }
+		
+
+		std::cout<<std::endl<<"#############################response end #######################"<<std::endl;
+
+		
+		
+		delete [] recvbuffer;
+		
+		
+
+	      
+	});
+
+    work.join();
+ #endif 
 	
-	 	
-	 	
-	
-	//socket.send(boost::asio::buffer())
+std::thread work1([&]{
 
+		std::cout<<"test  managementGroupModify msg ack Cache function"<<std::endl;
+		IM::Group::IMGroupManagementModifyReq msg;
+		IM::Group::IMGroupManagementModifyResp msgResp; 
+		//creator 
+		//msg.set_creator_id(1856727);
+
+		//manager
+		msg.set_creator_id(1856730);
+		msg.set_creator_name("xxxxxxxxxxxxcreator");
+		msg.set_group_id(6144);
+		//1856728
+		msg.set_group_id(6144);
+		msg.set_group_name("xxxxxxxgroup name");
+		msg.set_peer_id(3305421);//creator
 	
+		//msg.set_result_code(0);
+
+
+		CImPdu pdu;
+		pdu.SetPBMsg(&msg);
+		pdu.SetServiceId(SID_GROUP);                            
+		pdu.SetCommandId(CID_GROUP_MANAGEMENT_MODIFY_REQUEST);
+		socket.send(boost::asio::buffer(pdu.GetBuffer(),pdu.GetLength()));
+
+		//usleep(1000*100);
+		//vector<char>recvbuffer(1024,0);
+		try{
+			unsigned char *recvbuffer=new unsigned char[1024];
+			memset(recvbuffer,0,sizeof(unsigned char)*1024);
+
+			socket.receive(boost::asio::buffer(recvbuffer,1024));
+
+
+			msgResp.ParseFromArray(recvbuffer+16, 1024);
+
+			//std::cout<<recvbuffer<<std::endl;
+
+			std::cout<<std::endl<<"#############################response start#######################"<<std::endl;
+			std::cout<<"group  name :  "<<msgResp.group_name()<<std::endl;
+			std::cout<<"result code : "<<msgResp.result_code()<<std::endl;
+			std::cout<<"group    id : "<<msgResp.group_id()<<std::endl;
+			std::cout<<"creator  id : "<<msgResp.creator_id()<<std::endl;
+	        std::cout<<"peer   name : "<<msgResp.peer_name()<<std::endl;
+			for(auto i=0;i < msgResp.member_list_size();i++)
+			{
+			   std::cout<<"member index "<< i<<" imid "<<msgResp.member_list(i)<<" in group [ "<< msgResp.group_id()<<" ]"<<std::endl;
+			}
+
+
+			std::cout<<std::endl<<"#############################response end #######################"<<std::endl;
+
+
+
+			delete [] recvbuffer;
+		
+		}
+		catch(...){
+			 std::cout<<"some system error ocurred!!! please pay attention to it"<<std::endl;
+		}
+
+
+
+  
+});
 	 
      t.join();
 	 f.join();
-	 work.join();
+
 	 work1.join();
-	 work2.join();
-	 work3.join();
-	 work4.join();
+	
 
 	 
   }
